93a95dd0e8c668b92f84c9d8756ca831f683ad01
Quinn Murphy <gamefiend@github.com>
2022-03-14 12:37:58 -0400

    colorized mage output

--- magefile.go
+++ magefile.go
@@ -8,11 +8,12 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/fatih/color"
 	"github.com/magefile/mage/mg"
 )
 
 func Build() error {
-	fmt.Println("Building...")
+	fmt.Println(color.GreenString("Building..."))
 	cmd := exec.Command("go", "build", "-o", "./linkchecker", "./cmd/main.go")
 	if err := cmd.Run(); err != nil {
 		return err
@@ -35,7 +36,7 @@ func Install() error {
 }
 
 func Test() error {
-	fmt.Println("Testing...")
+	fmt.Println(color.BlackString("Testing..."))
 	cmd := exec.Command("sh", "-c", "go test -json |gotestdox")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -45,16 +46,18 @@ func Test() error {
 
 func Run() error {
 	mg.Deps(Build)
-	fmt.Println("Testing...")
+	fmt.Println(color.GreenString("Running..."))
 	cmd := exec.Command("./linkchecker", "-v", "https://thoughtcrime-games.ghost.io")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	return cmd.Run()
+	err := cmd.Run()
+	mg.Deps(Clean)
+	return err
 }
 
 func RunJSON() error {
 	mg.Deps(Build)
-	fmt.Println("Testing...")
+	fmt.Println(color.GreenString("Running with JSON Output..."))
 	output, err := exec.Command("./linkchecker", "-v", "-j", "https://thoughtcrime-games.ghost.io").Output()
 	if err != nil {
 		mg.Deps(Clean)
@@ -65,6 +68,13 @@ func RunJSON() error {
 	return nil
 }
 
+func Tidy() error {
+	fmt.Println(color.YellowString("Tidying..."))
+	cmd := exec.Command("go", "mod", "tidy")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
 func Clean() {
 	fmt.Println("Cleaning...")
 	os.RemoveAll("linkchecker")
